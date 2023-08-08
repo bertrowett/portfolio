69e339f78c8540e2eb1d20f865b219f9c5474516
Bert Rowett <bertrowett@googlemail.com>
2023-08-08 13:40:46 +0200

    Started adding Projects w/ JSON- titles, arrows + chevrons added

--- js/index.js
+++ js/index.js
@@ -4,7 +4,10 @@ fetch('projects.json')
     }).then((data) => {
         const projectsSection = document.querySelector('.projects')
 
+        let count = 1
         data.projects.forEach((project) => {
-            projectsSection.innerHTML += 'test'
+            projectsSection.innerHTML += '<h3 class="mt-4 text-xl turquoise"><span class="arrow_p' + count + ' mr-8 red"><i class="fa-solid fa-right-long"></i></span>' + project.name + '<i class="downchev_p' + count + ' fa-solid fa-chevron-down ml-24 white"></i><i class="upchev_p' + count + ' fa-solid fa-chevron-up ml-24 white hidden"></i></h3>'
+            
+            count += 1
         })
     })
